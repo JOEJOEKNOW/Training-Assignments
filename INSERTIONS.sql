dd549042c3f022804e5757af5774efd847e7d1d8
JOEJOEKNOW <98570089+JOEJOEKNOW@users.noreply.github.com>
2022-02-07 07:40:27 +0800

    Add files via upload

---
+++ INSERTIONS.sql
@@ -0,0 +1,17 @@
+insert into admins(email, password)  values('joejoe@gmail.com', 'password123');
+
+
+
+
+
+
+
+
+INSERT INTO categories (category_name) VALUES ('Motherboards');
+
+
+
+
+
+INSERT INTO products(product_id,product_name,category_id,product_price,product_image,product_available_qty) 
+VALUES ('A0001','ASUS','CAT 0000001','$1,200.00','ASUSMOBO.jpg','69');
